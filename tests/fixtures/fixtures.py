172ffd9f63bfc71b065a77715babab2d4827cf6d
krivitskyanton <krivitsky.anton@gmail.com>
2024-06-23 21:07:55 +0200

    linter fixes

--- tests/fixtures/fixtures.py
+++ tests/fixtures/fixtures.py
@@ -1,3 +1,2 @@
 result_file1_file2 = '{\n- follow: False\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ ' \
                      'verbose: True\n}'
-
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,4 +6,3 @@ def test_two_json():
     result = generate_diff('tests/files/file1.json', 'tests/files/file2.json')
     true_result = fixtures.result_file1_file2
     assert result == true_result
-
