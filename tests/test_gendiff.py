9d8cd5a97bfe80834e4d1f7c35fa250fecf23306
krivitskyanton <krivitsky.anton@gmail.com>
2024-07-10 20:07:10 +0200

    changes

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,40 +1,10 @@
-from gendiff.parser import generate_diff_json, generate_diff_yaml
-from tests.fixtures import fixtures
-
-FILE_PATH = 'tests/files/'
+import pytest
 
 
-def test_json_f1_f2():
-    result = generate_diff_json(f'{FILE_PATH}file1.json', f'{FILE_PATH}file2.json')
-    true_result = fixtures.result_f1_f2
-    assert result == true_result
+def test_generate_diff():
+    pass
 
 
-def test_json_f1_f3():
-    result = generate_diff_json(f'{FILE_PATH}file1.json', f'{FILE_PATH}file3.json')
-    true_result = fixtures.result_f1_f3
-    assert result == true_result
-
-
-def test_json_f3_f2():
-    result = generate_diff_json(f'{FILE_PATH}file3.json', f'{FILE_PATH}file2.json')
-    true_result = fixtures.result_f3_f2
-    assert result == true_result
-
-
-def test_yaml_f3_f2():
-    result = generate_diff_yaml(f'{FILE_PATH}file3.yml', f'{FILE_PATH}file2.yaml')
-    true_result = fixtures.result_f3_f2
-    assert result == true_result
-
-
-def test_yaml_f1_f2():
-    result = generate_diff_yaml(f'{FILE_PATH}file1.yml', f'{FILE_PATH}file2.yaml')
-    true_result = fixtures.result_f1_f2
-    assert result == true_result
-
-
-def test_yaml_f1_f3():
-    result = generate_diff_yaml(f'{FILE_PATH}file1.yml', f'{FILE_PATH}file3.yml')
-    true_result = fixtures.result_f1_f3
-    assert result == true_result
+def read_file(file_name):
+    with open(file_name, 'r') as file:
+        return file.read().strip()
